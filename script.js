28955d5d292a823ccb77b5b3fb30b629c37d62c9
ridanrekas <nadir.bensaker@gmail.com>
2025-07-05 22:39:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let matieresData = {};
-
 document.addEventListener("DOMContentLoaded", function() {
   fetch('data.json')
     .then(response => response.json())
     .then(data => {
-      matieresData = data;
+      window.matieresData = data; // variable globale accessible partout
       generateChoixForm(5);
     })
     .catch(error => {
@@ -14,6 +12,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function generateChoixForm(nbChoix) {
   const container = document.getElementById("choix-container");
+  container.innerHTML = ''; // vide au cas où
+
   for (let i = 1; i <= nbChoix; i++) {
     const obligatoire = i <= 3 ? ' (obligatoire)' : ' (facultatif)';
     const fieldset = document.createElement("fieldset");
@@ -37,7 +37,6 @@ function generateChoixForm(nbChoix) {
       <div id="nature${i}"></div>
     `;
     container.appendChild(fieldset);
-
     initDropdown(i);
   }
 }
@@ -49,21 +48,19 @@ function initDropdown(index) {
   const natureDiv = document.getElementById(`nature${index}`);
 
   // Remplir spécialités
-  for (const spec in matieresData) {
-    sp.innerHTML += `<option value="${spec}">${spec}</option>`;
-  }
+  sp.innerHTML += Object.keys(window.matieresData).map(spec => `<option value="${spec}">${spec}</option>`).join('');
 
   sp.onchange = () => {
     sem.innerHTML = '<option value="">-- Choisir un semestre --</option>';
     mat.innerHTML = '<option value="">-- Choisir une matière --</option>';
     natureDiv.innerHTML = '';
     const selectedSp = sp.value;
-    if (matieresData[selectedSp]) {
-      for (const s in matieresData[selectedSp]) {
-        if (/^S[1357]$/.test(s)) {
+    if (window.matieresData[selectedSp]) {
+      Object.keys(window.matieresData[selectedSp])
+        .filter(s => /^S[1357]$/.test(s))
+        .forEach(s => {
           sem.innerHTML += `<option value="${s}">${s}</option>`;
-        }
-      }
+        });
     }
   };
 
@@ -72,10 +69,10 @@ function initDropdown(index) {
     const selectedSem = sem.value;
     mat.innerHTML = '<option value="">-- Choisir une matière --</option>';
     natureDiv.innerHTML = '';
-    if (matieresData[selectedSp] && matieresData[selectedSp][selectedSem]) {
-      for (const obj of matieresData[selectedSp][selectedSem]) {
+    if (window.matieresData[selectedSp] && window.matieresData[selectedSp][selectedSem]) {
+      window.matieresData[selectedSp][selectedSem].forEach(obj => {
         mat.innerHTML += `<option value="${obj.matiere}">${obj.matiere}</option>`;
-      }
+      });
     }
   };
 
@@ -84,8 +81,8 @@ function initDropdown(index) {
     const selectedSem = sem.value;
     const selectedMat = mat.value;
     natureDiv.innerHTML = '';
-    if (matieresData[selectedSp] && matieresData[selectedSp][selectedSem]) {
-      const obj = matieresData[selectedSp][selectedSem].find(m => m.matiere === selectedMat);
+    if (window.matieresData[selectedSp] && window.matieresData[selectedSp][selectedSem]) {
+      const obj = window.matieresData[selectedSp][selectedSem].find(m => m.matiere === selectedMat);
       if (obj) {
         if (obj.cours) natureDiv.innerHTML += `<label><input type="checkbox" name="nature${index}[]" value="Cours"> Cours</label><br>`;
         if (obj.td) natureDiv.innerHTML += `<label><input type="checkbox" name="nature${index}[]" value="TD"> TD</label><br>`;
@@ -94,6 +91,7 @@ function initDropdown(index) {
     }
   };
 }
+
 
 
 
